f3aaa99d1c8758b85cb15030f998af297053be4a
e9e3 <1013584338@qq.com>
2024-04-05 23:06:49 +0800

    player封装 对于斜着走的情况进行优化

--- game-test-3/源.cpp
+++ game-test-3/源.cpp
@@ -4,21 +4,23 @@
 #include<vector>
 
 
-#define fps 30//游戏fps
+#define fps 240//游戏fps
 
 const int  PLAYER_ANIM_NUM = 6;//角色动画帧数
-const int PLAYER_SPEED = 3;//角色速度
 
-int idx_current_anim = 0;
+const int WINDOW_WIDTH = 1280;//窗口宽度
+const int WINDOW_HEIGHT = 720;//窗口高度
 
-IMAGE img_player_left[PLAYER_ANIM_NUM];//向左
-IMAGE img_player_right[PLAYER_ANIM_NUM];//向右
-POINT player_pos = { 500,500 };//角色位置
+int idx_current_anim = 0;//当前动画帧索引
 
 
 
-//用循环重载图像
-void LoadAnimation()
+IMAGE img_player_left[PLAYER_ANIM_NUM];
+IMAGE img_player_right[PLAYER_ANIM_NUM];
+
+
+
+void LoadAnimation()//加载动画
 {
 	for (size_t i = 0; i < PLAYER_ANIM_NUM; i++)
 	{
@@ -27,11 +29,10 @@ void LoadAnimation()
 	}
 	for (size_t i = 0; i < PLAYER_ANIM_NUM; i++)
 	{
-		std::wstring path = L"img/player_right" + std::to_wstring(i) + L".png";
+		std::wstring path = L"img/player_right_" + std::to_wstring(i) + L".png";
 		loadimage(&img_player_right[i], path.c_str());
 	}
 }
-
 
 
 //调整混合模式，解决角色黑框
@@ -43,9 +44,7 @@ inline void putimage_alpha(int x, int y, IMAGE* img)
 		GetImageHDC(img), 0, 0, w, h, { AC_SRC_OVER,0,255,AC_SRC_ALPHA });
 }
 
-
-
-class Animation //动画
+class Animation //动画类
 {
 public:
 	Animation(LPCTSTR path, int num, int interval)//路径命名方式 数量 帧间隔
@@ -57,7 +56,7 @@ public:
 
 		for (size_t i = 0; i < num; i++)
 		{
-			_stprintf_s(path_file, path, i);
+			_stprintf_s(path_file, path, i);//格式化字符串
 
 			IMAGE* frame = new IMAGE();//malloc
 			loadimage(frame, path_file);
@@ -70,7 +69,7 @@ public:
 		for (size_t i = 0; i < frame_list.size(); i++)//用size获取元素个数
 			delete frame_list[i];  //free
 	}
-	void Play(int x, int y, int delta)
+	void Play(int x, int y, int delta)//播放
 	{
 		timer += delta;//计时器
 		if (timer >= interval_ms)//计时大于delta
@@ -78,114 +77,181 @@ public:
 			idx_frame = (idx_frame + 1) % frame_list.size();//下一帧，取模防溢出
 			timer = 0;//计时器归零
 		}
-		putimage_alpha(x, y, frame_list[idx_frame]);
+		putimage_alpha(x, y, frame_list[idx_frame]);//渲染
 	}
+	
 private:
-	int timer = 0;
-	int idx_frame = 0;
-	int interval_ms = 0;
-	std::vector<IMAGE*> frame_list;
+	int timer = 0;//计时器
+	int idx_frame = 0;//帧索引
+	int interval_ms = 0;//帧间隔
+	std::vector<IMAGE*> frame_list;//帧列表
 };
 
+class Player
+{
+public:
+	Player()
+	{
+		loadimage(&img_shadow, _T("img/shawdow_player.png"));
+		anim_left = new Animation(_T("img/player_left_%d.png"), 6, 45);
+		anim_right = new Animation(_T("img/player_right_%d.png"), 6, 45);
+	}
+
+	~Player()
+	{
+		delete anim_left;
+		delete anim_right;
+	}
+
+	void Draw(int delta)
+	{
+		int pos_shadow_x = player_pos.x + (PLAYER_WIDTH - SHADOW_WIDTH) / 2;
+		int pos_shadow_y = player_pos.y - 8;
+		putimage_alpha(pos_shadow_x, pos_shadow_y, &img_shadow);
+		static bool facing_left = false;
+
+		int dir_x = is_move_right - is_move_left;//方向
+		if (dir_x > 0)
+			facing_left = false;
+		else if (dir_x < 0)
+			facing_left = true;
+
+		if (facing_left)
+			anim_left->Play(player_pos.x, player_pos.y, delta);
+		else
+			anim_right->Play(player_pos.x, player_pos.y, delta);
+	}
+
+	void ProcessEvent(const ExMessage& msg)//处理事件
+	{
+		switch (msg.message)
+		{
+		case WM_KEYDOWN:
+			switch (msg.vkcode)
+			{
+			case VK_UP:
+			case 0x57:
+				is_move_up = true;
+				break;
+
+			case VK_DOWN:
+			case 0x53:
+				is_move_down = true;
+				break;
+
+			case VK_LEFT:
+			case 0x41:
+				is_move_left = true;
+				break;
+
+			case VK_RIGHT:
+			case 0x44:
+				is_move_right = true;
+				break;
+			}
+			break;
+
+		case WM_KEYUP:
+			switch (msg.vkcode)
+			{
+			case VK_UP:
+			case 0x57:
+				is_move_up = false;
+				break;
+
+			case VK_DOWN:
+			case 0x53:
+				is_move_down = false;
+				break;
+
+			case VK_LEFT:
+			case 0x41:
+				is_move_left = false;
+				break;
+
+			case VK_RIGHT:
+			case 0x44:
+				is_move_right = false;
+				break;
+			}
+			break;
+		}
+	}
+
+	void Move()
+	{
+		int dir_x = is_move_right - is_move_left;//方向
+		int dir_y = is_move_down - is_move_up;
+
+		double len_dir = sqrt(dir_x * dir_x + dir_y * dir_y);//向量长度
+
+		if (len_dir != 0)//移动
+		{
+
+			double normalized_x = dir_x / len_dir;	//单位向量
+			double normalized_y = dir_y / len_dir;//单位向量
+
+			player_pos.x += (int)(normalized_x * PLAYER_SPEED);
+			player_pos.y += (int)(normalized_y * PLAYER_SPEED);
+
+		}
+		if (player_pos.x < 0) player_pos.x = 0;
+		if (player_pos.y < 0) player_pos.y = 0;
+		if (player_pos.x + PLAYER_WIDTH > WINDOW_WIDTH) player_pos.x = WINDOW_WIDTH - PLAYER_WIDTH;
+		if (player_pos.y + PLAYER_HEIGHT > WINDOW_HEIGHT) player_pos.y = WINDOW_HEIGHT - PLAYER_HEIGHT;
+	}
+private:
+
+	const int PLAYER_SPEED = 10;//角色速度
+	const int PLAYER_WIDTH = 80;//角色宽度
+	const int PLAYER_HEIGHT = 80;//角色高度
+	const int SHADOW_WIDTH = 32;
+
+	Animation* anim_left;
+	Animation* anim_right;
+
+	POINT player_pos{ 500, 500 };//角色位置
+
+	IMAGE img_shadow;
+
+	//人物各方向状态
+
+	bool is_move_up = false;
+	bool is_move_down = false;
+	bool is_move_left = false;
+	bool is_move_right = false;
+
+};
+
+Player player;
 
 int main()
 {
 	//初始化
 	initgraph(1280, 720);
-	BeginBatchDraw();
 	
 	ExMessage msg;
 
 	IMAGE img_background;
 
-
-	//人物各方向状态
-	bool is_up = false;
-	bool is_down = false;
-	bool is_right = false;
-	bool is_left = false;
-
 	LoadAnimation();
-
 	loadimage(&img_background, _T("img/background.png"));
 
+	BeginBatchDraw();
 
-
-
-
-
-
-	//主循环塞子
-	bool running = true;
+	bool running = true;//运行状态
 	while (running)
 	{
 		//开始时间戳
 		DWORD start_time = GetTickCount();
 
-
 		//轮询
 		while (peekmessage(&msg))
 		{
+			player.ProcessEvent(msg);
 			//按下键盘修改状态
-			if (msg.message == WM_KEYDOWN)
-			{
-				switch (msg.vkcode)
-				{
-				case VK_UP:
-				case 0x57:
-					is_up = true;
-					break;
-
-				case VK_DOWN:
-				case 0x53:
-					is_down = true;
-					break;
-
-				case VK_LEFT:
-				case 0x41:
-					is_left = true;
-					break;
-
-				case VK_RIGHT:
-				case 0x44:
-					is_right = true;
-					break;
-				}
-			}
-
-			//抬起键盘修改状态
-			else if (msg.message == WM_KEYUP)
-			{
-				switch (msg.vkcode)
-				{
-				case VK_UP:
-				case 0x57:
-					is_up = false;
-					break;
-
-				case VK_DOWN:
-				case 0x53:
-					is_down = false;
-					break;
-
-				case VK_LEFT:
-				case 0x41:
-					is_left = false;
-					break;
-
-				case VK_RIGHT:
-				case 0x44:
-					is_right = false;
-					break;
-				}
-			}
 		}
-
-			if (is_up) player_pos.y -= PLAYER_SPEED;
-			if (is_down) player_pos.y += PLAYER_SPEED;
-			if (is_left) player_pos.x -= PLAYER_SPEED;
-			if (is_right) player_pos.x += PLAYER_SPEED;
-
+		player.Move();
 
 		//动画帧循环索引
 		static int counter = 0;
@@ -197,7 +263,7 @@ int main()
 		cleardevice();
 
 		putimage(0, 0, &img_background);
-		putimage_alpha(player_pos.x, player_pos.y, &img_player_left[idx_current_anim]);
+		player.Draw(1000 / fps);
 
 		FlushBatchDraw();//结束时间戳+计算运行时间
 		DWORD end_time = GetTickCount();
@@ -209,7 +275,9 @@ int main()
 			Sleep(1000 / fps - delta_time);
 		}
 	}
-
 	EndBatchDraw();
-
+	//关闭图形窗口
+	closegraph();
+	return 0;
 }
+
