75d1230a851b671ddfac07793a5393b61345fe8a
e9e3 <1013584338@qq.com>
2024-04-06 18:18:34 +0800

    新增敌人 子弹类 修改动态休眠机制

--- game-test-3/源.cpp
+++ game-test-3/源.cpp
@@ -3,21 +3,17 @@
 #include <wingdi.h>
 #include<vector>
 
-
 #define fps 240//游戏fps
 
-const int  PLAYER_ANIM_NUM = 6;//角色动画帧数
+const int  PLAYER_ANIM_NUM = 6;//角色动画帧数+1
 
-const int WINDOW_WIDTH = 1280;//窗口宽度
-const int WINDOW_HEIGHT = 720;//窗口高度
+const int WINDOW_WIDTH = 1024;//窗口宽度
+const int WINDOW_HEIGHT = 900;//窗口高度
 
 int idx_current_anim = 0;//当前动画帧索引
 
-
-
 IMAGE img_player_left[PLAYER_ANIM_NUM];
 IMAGE img_player_right[PLAYER_ANIM_NUM];
-
 
 
 void LoadAnimation()//加载动画
@@ -33,7 +29,6 @@ void LoadAnimation()//加载动画
 		loadimage(&img_player_right[i], path.c_str());
 	}
 }
-
 
 //调整混合模式，解决角色黑框
 inline void putimage_alpha(int x, int y, IMAGE* img)
@@ -79,7 +74,7 @@ public:
 		}
 		putimage_alpha(x, y, frame_list[idx_frame]);//渲染
 	}
-	
+
 private:
 	int timer = 0;//计时器
 	int idx_frame = 0;//帧索引
@@ -178,6 +173,7 @@ public:
 		}
 	}
 
+
 	void Move()
 	{
 		int dir_x = is_move_right - is_move_left;//方向
@@ -200,17 +196,20 @@ public:
 		if (player_pos.x + PLAYER_WIDTH > WINDOW_WIDTH) player_pos.x = WINDOW_WIDTH - PLAYER_WIDTH;
 		if (player_pos.y + PLAYER_HEIGHT > WINDOW_HEIGHT) player_pos.y = WINDOW_HEIGHT - PLAYER_HEIGHT;
 	}
+	const POINT& GetPosition()const
+	{
+		return player_pos;
+	}
 private:
+	POINT player_pos{ 500, 500 };//角色位置
 
-	const int PLAYER_SPEED = 10;//角色速度
+	const int PLAYER_SPEED = 6;//角色速度
 	const int PLAYER_WIDTH = 80;//角色宽度
 	const int PLAYER_HEIGHT = 80;//角色高度
 	const int SHADOW_WIDTH = 32;
 
 	Animation* anim_left;
 	Animation* anim_right;
-
-	POINT player_pos{ 500, 500 };//角色位置
 
 	IMAGE img_shadow;
 
@@ -225,17 +224,166 @@ private:
 
 Player player;
 
+class Bullet
+{
+public:
+	POINT position = { 0,0 };
+
+public:
+	Bullet() = default;
+	~Bullet() = default;
+	void Draw() const
+	{
+		setlinecolor(RGB(255, 155, 50));
+		setfillcolor(RGB(171, 84, 90));
+		fillcircle(position.x, position.y, RADIUS);
+	}
+
+private:
+	const int RADIUS = 10;
+
+};
+
+Bullet bullet;
+
+class Enemy
+{
+public:
+	Enemy()
+	{
+		loadimage(&img_shadow, _T("img/shawdow_enemy.png"));
+		anim_left = new Animation(_T("img/enemy_left_%d.png"), 6, 45);
+		anim_right = new Animation(_T("img/enemy_right_%d.png"), 6, 45);
+
+		enum class SpawnEdge
+		{
+			Up = 0,
+			Down,
+			Left,
+			Right
+		};
+		SpawnEdge edge = (SpawnEdge)(rand() % 4);//随机生成边界
+
+		switch (edge)
+		{
+		case SpawnEdge::Up:
+
+			position.x = rand() % WINDOW_WIDTH;
+			position.y = -FRAME_HEIGHT;
+			break;
+
+
+		case SpawnEdge::Down:
+
+			position.x = rand() % WINDOW_WIDTH;
+			position.y = WINDOW_HEIGHT;
+			break;
+
+
+		case SpawnEdge::Left:
+
+			position.x = -FRAME_WIDTH;
+			position.y = rand() % WINDOW_HEIGHT;
+			break;
+
+
+		case SpawnEdge::Right:
+
+			position.x = WINDOW_WIDTH;
+			position.y = rand() % WINDOW_HEIGHT;
+			break;
+
+
+		}
+	}
+
+	~Enemy()
+	{
+		delete anim_left;
+		delete anim_right;
+	}
+
+	bool CheckBullerCollisiion(const Bullet& bullet)
+	{
+		return false;
+	}
+
+	bool  CheckPlayerCollision(const Player& player)
+	{
+		return false;
+	}
+
+	void Move(const Player& player)
+	{
+		const POINT& player_pos = player.GetPosition();
+
+		int dir_x = player_pos.x - position.x;
+		int dir_y = player_pos.y - position.y;
+
+		double len_dir = sqrt(dir_x * dir_x + dir_y * dir_y);//向量长度
+		if (len_dir != 0)
+		{
+			double normalized_x = dir_x / len_dir;	//单位向量
+			double normalized_y = dir_y / len_dir;//单位向量
+
+			position.x += (int)(normalized_x * SPEED);
+			position.y += (int)(normalized_y * SPEED);
+
+			if (dir_x < 0)
+				facing_left = true;
+			else if (dir_x > 0)
+				facing_left = false;
+		}
+
+	}
+	void Draw(int delta)
+	{
+		int pos_shadow_x = position.x + (FRAME_WIDTH - SHADOW_WIDTH) / 2;
+		int pos_shadow_y = position.y - 35;
+
+		putimage_alpha(pos_shadow_x, pos_shadow_y, &img_shadow);
+
+		if (facing_left)
+			anim_left->Play(position.x, position.y, delta);
+		else
+			anim_right->Play(position.x, position.y, delta);
+
+	}
+
+private:
+	const int SPEED = 2;
+	const int FRAME_WIDTH = 80;
+	const int FRAME_HEIGHT = 80;
+	const int SHADOW_WIDTH = 48;
+
+private:
+	IMAGE img_shadow;
+	Animation* anim_left;
+	Animation* anim_right;
+	POINT position{ 0,0 };
+	bool facing_left = false;
+};
+
+void TryGenerateEnemy(std::vector<Enemy*>& enemy_list)
+{
+	const int INTERVAL = 100;
+	static int counter = 0;
+	if ((++counter) % INTERVAL == 0)
+		enemy_list.push_back(new Enemy());
+}
+
 int main()
 {
 	//初始化
-	initgraph(1280, 720);
-	
+	initgraph(1024,900);
+
 	ExMessage msg;
 
 	IMAGE img_background;
+	std::vector<Enemy*> enemy_list;;
 
 	LoadAnimation();
-	loadimage(&img_background, _T("img/background.png"));
+	loadimage(&img_background, _T("img/background_1.png"));
 
 	BeginBatchDraw();
 
@@ -244,15 +392,16 @@ int main()
 	{
 		//开始时间戳
 		DWORD start_time = GetTickCount();
-
+		static DWORD delta_time = 1000 / fps;
 		//轮询
 		while (peekmessage(&msg))
 		{
 			player.ProcessEvent(msg);
-			//按下键盘修改状态
 		}
 		player.Move();
-
+		TryGenerateEnemy(enemy_list);
+		for (Enemy* enemy : enemy_list)
+			enemy->Move(player);
 		//动画帧循环索引
 		static int counter = 0;
 		if (++counter % 5 == 0)
@@ -265,15 +414,22 @@ int main()
 		putimage(0, 0, &img_background);
 		player.Draw(1000 / fps);
 
-		FlushBatchDraw();//结束时间戳+计算运行时间
-		DWORD end_time = GetTickCount();
-		DWORD delta_time = end_time - start_time;
+		for (Enemy* enemy : enemy_list)
+			enemy->Draw(1000 / 144);
+		/*	for (const Bullet& bullet : bullet_list)
+				bullet.Draw();*/
 
+		FlushBatchDraw();//结束时间戳+计算运行时间
+
+		DWORD end_time = GetTickCount();
+
+		delta_time = end_time - start_time;
 		//动态休眠
 		if (delta_time < 1000 / fps)
 		{
 			Sleep(1000 / fps - delta_time);
 		}
+
 	}
 	EndBatchDraw();
 	//关闭图形窗口
