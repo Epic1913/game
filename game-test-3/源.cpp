08f3773dd9f26b3257defa163b95e5d9df25ed8e
e9e3 <1013584338@qq.com>
2024-04-07 21:47:08 +0800

    新增子弹绘制 碰撞检测

--- game-test-3/源.cpp
+++ game-test-3/源.cpp
@@ -2,8 +2,11 @@
 #include<string>
 #include <wingdi.h>
 #include<vector>
+#include <chrono>
+#include <thread>
+#pragma comment(lib,"MSIMG32.LIB")
 
-#define fps 240//游戏fps
+const int fps=240;//游戏fps
 
 const int  PLAYER_ANIM_NUM = 6;//角色动画帧数+1
 
@@ -14,7 +17,6 @@ int idx_current_anim = 0;//当前动画帧索引
 
 IMAGE img_player_left[PLAYER_ANIM_NUM];
 IMAGE img_player_right[PLAYER_ANIM_NUM];
-
 
 void LoadAnimation()//加载动画
 {
@@ -39,7 +41,7 @@ inline void putimage_alpha(int x, int y, IMAGE* img)
 		GetImageHDC(img), 0, 0, w, h, { AC_SRC_OVER,0,255,AC_SRC_ALPHA });
 }
 
-class Animation //动画类
+class Animation
 {
 public:
 	Animation(LPCTSTR path, int num, int interval)//路径命名方式 数量 帧间隔
@@ -85,9 +87,13 @@ private:
 class Player
 {
 public:
+	const int PLAYER_WIDTH = 80;//角色宽度
+	const int PLAYER_HEIGHT = 80;//角色高度
+
+public:
 	Player()
 	{
-		loadimage(&img_shadow, _T("img/shawdow_player.png"));
+		loadimage(&img_shadow, _T("img/shadow_player.png"));//拼写错误
 		anim_left = new Animation(_T("img/player_left_%d.png"), 6, 45);
 		anim_right = new Animation(_T("img/player_right_%d.png"), 6, 45);
 	}
@@ -101,7 +107,7 @@ public:
 	void Draw(int delta)
 	{
 		int pos_shadow_x = player_pos.x + (PLAYER_WIDTH - SHADOW_WIDTH) / 2;
-		int pos_shadow_y = player_pos.y - 8;
+		int pos_shadow_y = player_pos.y + PLAYER_HEIGHT;
 		putimage_alpha(pos_shadow_x, pos_shadow_y, &img_shadow);
 		static bool facing_left = false;
 
@@ -174,7 +180,7 @@ public:
 	}
 
 
-	void Move()
+	void Move()//人物移动
 	{
 		int dir_x = is_move_right - is_move_left;//方向
 		int dir_y = is_move_down - is_move_up;
@@ -203,9 +209,7 @@ public:
 private:
 	POINT player_pos{ 500, 500 };//角色位置
 
-	const int PLAYER_SPEED = 6;//角色速度
-	const int PLAYER_WIDTH = 80;//角色宽度
-	const int PLAYER_HEIGHT = 80;//角色高度
+	const int PLAYER_SPEED = 3;//角色速度
 	const int SHADOW_WIDTH = 32;
 
 	Animation* anim_left;
@@ -214,15 +218,11 @@ private:
 	IMAGE img_shadow;
 
 	//人物各方向状态
-
 	bool is_move_up = false;
 	bool is_move_down = false;
 	bool is_move_left = false;
 	bool is_move_right = false;
-
 };
-
-Player player;
 
 class Bullet
 {
@@ -243,8 +243,6 @@ private:
 	const int RADIUS = 10;
 
 };
-
-Bullet bullet;
 
 class Enemy
 {
@@ -272,13 +270,11 @@ public:
 			position.y = -FRAME_HEIGHT;
 			break;
 
-
 		case SpawnEdge::Down:
 
 			position.x = rand() % WINDOW_WIDTH;
 			position.y = WINDOW_HEIGHT;
 			break;
-
 
 		case SpawnEdge::Left:
 
@@ -286,13 +282,11 @@ public:
 			position.y = rand() % WINDOW_HEIGHT;
 			break;
 
-
 		case SpawnEdge::Right:
 
 			position.x = WINDOW_WIDTH;
 			position.y = rand() % WINDOW_HEIGHT;
 			break;
-
 
 		}
 	}
@@ -305,12 +299,19 @@ public:
 
 	bool CheckBullerCollisiion(const Bullet& bullet)
 	{
-		return false;
+		bool is_overlap_x= bullet.position.x  >= position.x && bullet.position.x <= position.x + FRAME_WIDTH;
+		bool is_overlap_y = bullet.position.y >= position.y && bullet.position.y <= position.y + FRAME_HEIGHT;
+		return is_overlap_x && is_overlap_y;
 	}
 
 	bool  CheckPlayerCollision(const Player& player)
 	{
-		return false;
+		POINT check_position = { position.x + FRAME_WIDTH / 2,position.y + FRAME_HEIGHT/2 };
+		POINT player_position = player.GetPosition();
+
+		bool is_overlap_x = check_position.x >= player_position.x && check_position.x <= player_position.x + FRAME_WIDTH;
+		bool is_overlap_y = check_position.y >= player_position.y && check_position.y <= player_position.y + FRAME_HEIGHT;
+		return is_overlap_x && is_overlap_y;
 	}
 
 	void Move(const Player& player)
@@ -349,12 +350,19 @@ public:
 			anim_right->Play(position.x, position.y, delta);
 
 	}
-
+	void hurt()
+	{
+		alive = false;
+	}
+	bool Checkalive()
+	{
+		return alive;
+	}
 private:
-	const int SPEED = 2;
-	const int FRAME_WIDTH = 80;
-	const int FRAME_HEIGHT = 80;
-	const int SHADOW_WIDTH = 48;
+	const int SPEED = 2;//速度
+	const int FRAME_WIDTH = 80;//帧宽度
+	const int FRAME_HEIGHT = 80;//帧高度
+	const int SHADOW_WIDTH = 48;//阴影宽度
 
 private:
 	IMAGE img_shadow;
@@ -362,74 +370,118 @@ private:
 	Animation* anim_right;
 	POINT position{ 0,0 };
 	bool facing_left = false;
+	bool alive = true;
 };
 
+//生成敌人
 void TryGenerateEnemy(std::vector<Enemy*>& enemy_list)
 {
-	const int INTERVAL = 100;
+	const int INTERVAL = 50000/fps;
 	static int counter = 0;
-	if ((++counter) % INTERVAL == 0)
-		enemy_list.push_back(new Enemy());
+	if ((++counter) % INTERVAL == 0)//每隔一段时间生成敌人
+		enemy_list.push_back(new Enemy());//创建敌人
 }
+//子弹位置
+void UpdateBullets(std::vector<Bullet>& bullet_list,const Player& player)
+{
+	const double RADIAL_SPEED = 0.0045;
+	const double TANGENT_SPEED = 0.0055;
+	double radin_interval = 2 * 3.1415926535 / bullet_list.size();//
+	POINT player_position = player.GetPosition();
+	double radius = 100 + 25 * sin(GetTickCount() * RADIAL_SPEED);
+	for (size_t i = 0; i < bullet_list.size(); i++)
+	{
+		double radian = GetTickCount() * TANGENT_SPEED + radin_interval * i;
+		bullet_list[i].position.x = player_position.x + player.PLAYER_WIDTH / 2 + (int)(radius * sin(radian));
+		bullet_list[i].position.y = player_position.y + player.PLAYER_HEIGHT / 2 + (int)(radius * cos(radian));
+	}
 
-int main()
+}int main()
 {
 	//初始化
 	initgraph(1024,900);
-
+	auto next_frame = std::chrono::steady_clock::now();
+	
 	ExMessage msg;
+	Player player;
+	Bullet bullet;
 
 	IMAGE img_background;
-	std::vector<Enemy*> enemy_list;;
-
+	std::vector<Enemy*> enemy_list;
+	std::vector<Bullet> bullet_list(2);
 	LoadAnimation();
 	loadimage(&img_background, _T("img/background_1.png"));
 
 	BeginBatchDraw();
-
 	bool running = true;//运行状态
 	while (running)
-	{
+	{	
 		//开始时间戳
-		DWORD start_time = GetTickCount();
-		static DWORD delta_time = 1000 / fps;
+		/*DWORD start_time = GetTickCount();*/
+
+		auto start_time = std::chrono::steady_clock::now();
+
 		//轮询
 		while (peekmessage(&msg))
 		{
 			player.ProcessEvent(msg);
 		}
+		//角色移动
 		player.Move();
+		//更新子弹位置
+		UpdateBullets(bullet_list, player);
+		//生成敌人
 		TryGenerateEnemy(enemy_list);
+		
+		//敌人移动
 		for (Enemy* enemy : enemy_list)
 			enemy->Move(player);
-		//动画帧循环索引
-		static int counter = 0;
-		if (++counter % 5 == 0)
-			idx_current_anim++;
-		idx_current_anim %= 6;
+
+		//碰撞检测
+		for (Enemy* enemy : enemy_list)
+		{
+			if (enemy->CheckPlayerCollision(player))
+			{
+				MessageBox(GetHWnd(), _T("点此退出"), _T("Game Over"), MB_OK);
+				running = false;
+				break;
+			}
+		}
 
 		//二重缓冲+背景图片
 		cleardevice();
-
 		putimage(0, 0, &img_background);
-		player.Draw(1000 / fps);
+		player.Draw(1000/fps);
 
 		for (Enemy* enemy : enemy_list)
-			enemy->Draw(1000 / 144);
+			enemy->Draw(1000 / fps);
+		for(const Bullet& bullet:bullet_list)
+			bullet.Draw();
 		/*	for (const Bullet& bullet : bullet_list)
 				bullet.Draw();*/
 
 		FlushBatchDraw();//结束时间戳+计算运行时间
 
-		DWORD end_time = GetTickCount();
+		//DWORD end_time = GetTickCount();
+		auto end_time = std::chrono::steady_clock::now();
+		//DWORD delta_time = end_time - start_time;
+		////动态休眠
+		//if (delta_time < 1000 / fps)
+		//{
+		//	Sleep(1000 / fps - delta_time);
+		//}
+		 // 计算本帧消耗的时间
+		auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+		auto target_frame_duration = std::chrono::milliseconds(1000 / fps);
 
-		delta_time = end_time - start_time;
-		//动态休眠
-		if (delta_time < 1000 / fps)
+		// 如果实际消耗时间小于目标帧时间，则等待差值
+		if (elapsed < target_frame_duration) 
 		{
-			Sleep(1000 / fps - delta_time);
+			std::this_thread::sleep_for(target_frame_duration - elapsed);
 		}
 
+		// 更新下一帧的时间
+		next_frame += target_frame_duration;
 	}
 	EndBatchDraw();
 	//关闭图形窗口
